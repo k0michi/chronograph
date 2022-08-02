d1993c6a0365b4933287451adbfc29fdf17f7cdf
Komichi <k0michi@koyomi.co>
2022-08-02 23:31:50 +0900

    Add license notice

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path';
+import license from 'rollup-plugin-license';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  base: './',
+  plugins: [
+    license({
+      banner: {
+        content: `Please see /toolbox/license.txt for license information`,
+        commentStyle: 'ignored',
+      },
+      thirdParty: {
+        includePrivate: false,
+        output: {
+          file: path.join('dist', 'license.txt')
+        },
+      },
+    }),
+    react()
+  ]
 });
